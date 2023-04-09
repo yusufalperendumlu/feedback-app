17c76a85ec5ee6bf57b3e7b3538c0a7e0ec19b82
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-04-09 23:35:46 +0300

    fead: about me page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import AboutPage from "./pages/AboutPage";
+
+import IFeedbackData from "./data/IFeedbackData";
+
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
-import IFeedbackData from "./data/IFeedbackData";
 import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
@@ -20,15 +25,21 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
 
       <div className="comment">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Route exact path="/">
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        </Route>
+
+        <Routes>
+          <Route path="/about" Component={AboutPage} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
